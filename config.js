647fa0faec007d04943f27430906b74f3afc6b19
XaLAFREAK <88252600+XaLAFREAK@users.noreply.github.com>
2021-08-01 20:45:39 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
 hostedBy: true, 
 prefix: "!g",
 OwnerID: "790265200770809857", 
-token: "ODIwMjQ4OTE5ODM2OTgzMzA3.YEyaVQ.SLKhV4nKN2fS2LSgvP7eqqLPu_E",
+token: "ODIwMjQ4OTE5ODM2OTgzMzA3.YEyaVQ.SLKhV4nKN2fS2LSgvP7eqqLPu_EODcxNDQ3NTE4ODQ2NzgzNTM4.YQbcvw.ZTWDnMNbQXCzizMtqtXv12qZRHg",
 support: "4vthBEKHsB",
 basiclang: "en",
 color: "",
